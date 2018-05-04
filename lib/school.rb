048de639551d9e8ecc629df4da51060c8d7b102b
Kayla Reopelle <kreopelle@gmail.com>
2018-05-04 00:53:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,8 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
     if @roster.include?(grade) == false
-      
+      @roster[grade] = []
+      @roster[grade] << name
 
 end
